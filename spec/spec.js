8c90f4eda593c8cc702fd0eb1ef1c81dee40e6aa
Richard Mehlinger <richardm@dropbox.com>
2017-07-27 19:10:19 -0700

    working for now

--- spec/spec.js
+++ spec/spec.js
@@ -1,5 +1,5 @@
 jasmine.CATCH_EXCEPTIONS = false;
-import {DepFireTailCell} from '../src/main.js';
+import {DepFireTailCell, DepFireTailList, RWFireTailCell, RWFireTailList} from '../src/main.js';
 import * as firebase from 'firebase';
 import * as rx from 'bobtail-rx';
 
@@ -14,7 +14,7 @@ let app = firebase.initializeApp({
 });
 let db = app.database();
 
-// Error.stackTraceLimit = Infinity;
+Error.stackTraceLimit = 10;
 
 
 describe('DepFireTailCell', () => {
@@ -42,7 +42,7 @@ describe('DepFireTailCell', () => {
       Joe: {id: 0, age: 50},
       Fred: {id: 1, age: 40},
       Bob: {id: 2, age: 60}
-    })
+    });
   });
   it('should update when Firebase ref is deleted', () => {
     fieldVal().remove();
@@ -53,3 +53,86 @@ describe('DepFireTailCell', () => {
     expect(peopleCell.data).toEqual({"Maple": {id: 0, age: 2}});
   });
 });
+
+describe('DepFireTailList', () => {
+  let fieldVal, fieldKey, peopleCell, people, cats;
+
+  beforeEach(() => {
+    fieldKey = rx.cell("people");
+    fieldVal = () => db.ref(fieldKey.get());
+    people = db.ref('people');
+    people.set({});
+    cats = db.ref('cats');
+    cats.set({});
+    db.ref('people').push({name: "Joe", id: 0, age: 50});
+    db.ref('cats').push({name: "Maple", id: 0, age: 2});
+    db.ref('people').once('value').then(v => console.info(v.val()));
+    peopleCell = new DepFireTailList(fieldVal);
+  });
+  it('should update when Firebase children of ref change', () => {
+    expect(Object.values(peopleCell.data)).toEqual([{name: "Joe", id: 0, age: 50}]);
+    people.push({name: "Fred", id: 1, age: 40});
+    people.push({name: "Bob", id: 2, age: 60});
+    expect(Object.values(peopleCell.data)).toEqual([
+      {name: "Joe", id: 0, age: 50},
+      {name: "Fred", id: 1, age: 40},
+      {name: "Bob", id: 2, age: 60}
+    ]);
+  });
+  it('should update when Firebase ref is deleted', () => {
+    people.remove();
+    expect(peopleCell.data).toBeNull();
+  });
+  it('should update when its ref changes', () => {
+    fieldKey.set('cats');
+    expect(Object.values(peopleCell.data)).toEqual([{name: "Maple", id: 0, age: 2}]);
+  });
+});
+
+// describe('RWFireTailList', () => {
+//   let fieldVal, fieldKey, peopleCell, people, cats;
+//
+//   beforeEach(() => {
+//     fieldKey = rx.cell("people");
+//     fieldVal = () => db.ref(fieldKey.get());
+//     people = db.ref('people');
+//     people.set({});
+//     cats = db.ref('cats');
+//     cats.set({});
+//     db.ref('people').push({name: "Joe", id: 0, age: 50});
+//     db.ref('cats').push({name: "Maple", id: 0, age: 2});
+//     peopleCell = new RWFireTailList(fieldVal, data => data.orderByKey());
+//   });
+//
+//   it('should update firebase on writes', () => {
+//
+//   });
+// });
+//
+// describe('RWFireTailCell', () => {
+//   let fieldVal, fieldKey, peopleCell;
+//
+//   beforeEach(() => {
+//     fieldKey = rx.cell("people");
+//     fieldVal = () => {
+//       let ref = db.ref(fieldKey.get());
+//       console.info(fieldKey.get());
+//       return ref;
+//     };
+//     db.ref('people').set({"Joe": {id: 0, age: 50}});
+//     db.ref('cats').set({"Maple": {id: 0, age: 2}});
+//     peopleCell = new RWFireTailCell(fieldVal);
+//   });
+//   it('should update when Firebase value of ref changes', () => {
+//     expect(Object.values(peopleCell.data)).toEqual([{name: "Joe", id: 0, age: 50}]);
+//     people.push({name: "Fred", id: 1, age: 40});
+//     people.push({name: "Bob", id: 2, age: 60});
+//     expect(Object.values(peopleCell.data)).toEqual([
+//       {name: "Joe", id: 0, age: 50},
+//       {name: "Fred", id: 1, age: 40},
+//       {name: "Bob", id: 2, age: 60}
+//     ]);
+//   });
+//   it('should update Firebase when it is mutated', () => {
+//   });
+// });
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {autoSub, bind, snap} from 'bobtail-rx';
 import {ObsJsonCell, SrcJsonCell} from 'bobtail-json-cell';
 
 let valueEvent = {value(data){
+  console.warn(this.data, data.val());
   this._update(data.val());
 }};
 let listEvents = {
@@ -16,51 +17,84 @@ let listEvents = {
   }
 };
 
-export class FireTailBase extends ObsJsonCell {
-  constructor(f, init=null, events) {
-    // super(typeof f === 'function' ? f: () => f, init);
+function identity(x) {return x;}
+
+class FireTailBase extends ObsJsonCell {
+  constructor(refFn, xformFn=identity, init=null, events=valueEvent) {
+    // super(typeof refFn === 'function' ? refFn: () => refFn, init);
     super(init);
-    this.f = f;
+    this.refFn = refFn;
+    this.xformFn = xformFn;  // used for e.g.
     this.events = events;
-    this.firebaseRef = bind(this.f);
-    autoSub(this.firebaseRef.onSet, ([o, n]) => {
-      // console.info(o, n);
+    this.refCell = bind(this.refFn);
+    autoSub(this.refCell.onSet, ([o, n]) => {
       if(o) {
         o.off();  // turn off old listeners, if any
       }
       let entries = Object.entries(this.events);
-      entries.forEach(([ev, handler]) => n.on(ev, handler.bind(this)));
+      this._updating(() => entries.forEach(([ev, handler]) => n.on(ev, handler.bind(this))));
     });
   }
 }
 
+class DepFireTailBase extends FireTailBase {
+  constructor() {
+    super(...arguments);
+  }
+  // FIXME: figure out some way to do this via inheritance rather than copy-paste from bobtail-json-cell.
+  setProperty(getPath, basePath, obj, prop, val) {
+    if (!this._nowUpdating) {
+      throw new DepMutationError("Cannot mutate DepJsonCell!");
+    }
+    else return super.setProperty(getPath, basePath, obj, prop, val);
+  }
+
+  deleteProperty(getPath, basePath, obj, prop) {
+    if (!this._nowUpdating) {
+      throw new DepMutationError("Cannot mutate DepJsonCell!");
+    }
+    else return super.deleteProperty(getPath, basePath, obj, prop);
+  }
+}
+
 export class DepFireTailCell extends FireTailBase {
-  constructor(f, init=null) {
-    super(f, init, valueEvent);
+  constructor(refFn, init=null) {
+    super(refFn, identity, init, valueEvent);
   }
 }
 
 export class DepFireTailList extends DepFireTailCell {
-  constructor(f, init) {
-    super(f, init);
-    this.events = listEvents;
+  constructor(refFn, xformFn, init) {
+    super(refFn, xformFn, init, listEvents);
   }
 }
 
-export class RWFireTailBase extends SrcJsonCell {
-  constructor(firebaseRef, init, events) {
+export class RWFireTailBase extends FireTailBase {
+  constructor(refFn, xformFn, init, events) {
     super(init);
+    initFireTailObj.call(this, refFn, xformFn, events);
+  }
+  setProperty (getPath, basePath, obj, prop, val) {
+    // super.setProperty(getPath, basePath, obj, prop, val);
+    this.refCell.raw().update({[getPath(prop).join('/')]: val});
+  }
+  deleteProperty (getPath, basePath, obj, prop) {
+    // super.deleteProperty(getPath, basePath, obj, prop);
+    this.refCell.raw().remove(getPath(prop).join('/'));
+  }
+  push (elem) {
+    return this.refCell.raw().push(elem);
   }
 }
 
-export class RWFireTailCell extends SrcJsonCell {
-  constructor(firebaseRef, init) {
-    super(init, valueEvent);
+export class RWFireTailCell extends RWFireTailBase {
+  constructor(refFn, init) {
+    super(refFn, identity, init, valueEvent);
   }
 }
 
-export class SrcFireTailArray extends RWFireTailBase {
-  constructor (init) {
-    super(init, listEvents);
+export class RWFireTailArray extends RWFireTailBase {
+  constructor (refFn, init, xformFn) {
+    super(refFn, xformFn, init, valueEvent);
   }
 }
